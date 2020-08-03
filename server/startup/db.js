8deea8d97650d7ac0e5ccbefbd7cd55201a47e92
Michael Davidson <myke2424@gmail.com>
2020-08-02 21:26:06 -0400

    added travis ci

--- server/startup/db.js
+++ server/startup/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function () {
+  const db = config.get('db');
   mongoose
-    .connect('mongodb://localhost/bjj_log', {
+    .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log('Successfully connected to bjj log DB...'))
+    .then(() => console.log(`Successfully connected to ${db}`))
     .catch((err) => console.log('Couldnt connect to DB...', err));
 };
