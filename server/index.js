f6936fa189bcab53494e529d66513b7c02ca7288
Michael Davidson <myke2424@gmail.com>
2020-08-08 15:28:01 -0400

    added dotenv

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const port = process.env.port || 3000;
 
 require('./startup/routes')(app);
 require('./startup/db')(); // Initalize DB Connection
-require('./startup/config')(); // Set env variables
 
 const server = app.listen(port, () =>
   console.log(`Server running on port ${port}`)
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+require('dotenv').config();
 
 // Validate JWT - If valid - Give access to API Endpoint
 module.exports = function (req, res, next) {
+  const jwtPrivateKey = process.env.JWT_KEY;
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).send('Access Denied');
 
   try {
     // if the token is valid - return the decoded payload
-    const payload = jwt.verify(token, config.get('jwtPrivateKey'));
+    const payload = jwt.verify(token, jwtPrivateKey);
     req.user = payload;
     next();
   } catch (ex) {
